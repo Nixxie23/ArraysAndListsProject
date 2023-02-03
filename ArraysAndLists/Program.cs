8dbad7a8e32b29101b49e755e94531c62ac7529e
taylor Schuchter <taylorschuchter@gmail.com>
2023-02-03 18:18:17 -0500

    exercise complete

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -6,6 +7,29 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
+            int[] numOneToTen = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> evens= new List<int>();
+            List<int> odds= new List<int>();
+
+            foreach (int num in numOneToTen)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                { 
+                    odds.Add(num);
+            }
+            }
+            foreach (int evenNum in evens)
+            {
+                Console.WriteLine($"{evenNum} is an even number!");
+            }
+            foreach (int oddNum in odds)    
+            {
+                Console.WriteLine($"{oddNum} can't even! That means it must be odd!");
+            }
             //TODO:
 
             // Create an int Array and populate numbers 1-10
